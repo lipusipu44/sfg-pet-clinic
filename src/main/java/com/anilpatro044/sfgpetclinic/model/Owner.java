bc1cc5be09b4411d12d24ae03bd013857e0f186d
lipusipu44 <anilpatro044@gmail.com>
2020-12-12 20:34:11 +0530

    First push

---
+++ src/main/java/com/anilpatro044/sfgpetclinic/model/Owner.java
@@ -0,0 +1,5 @@
+package com.anilpatro044.sfgpetclinic.model;
+
+public class Owner extends Person{
+
+}
---
+++ src/main/java/com/anilpatro044/sfgpetclinic/model/Vet.java
@@ -0,0 +1,5 @@
+package com.anilpatro044.sfgpetclinic.model;
+
+public class Vet extends Person{
+
+}
